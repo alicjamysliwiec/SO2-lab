917fb714dc780ea8867c909ffc1cc014bb91ff3a
amysliwiec248867 <amysliwiec248867@e-science.pl>
2021-04-06 23:55:12 +0200

    Create Skrypt2.sh

---
+++ Lab2/Skrypt2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eu
+
+#Kody błędów
+DIRECTORIES_DO_NOT_EXIST=14
+NO_PARAMETERS_WERE_GIVEN=27
+
+# Skrypt pobiera dwa argumenty - dwie ścieżki do katalogów
+DIRECTORY=${1}
+FILE=${2}
+
+#Jeżeli nie podamy parametrów -eu??
+if [[ $# -lt 2 ]]; then
+	exit ${NO_PARAMETERS_WERE_GIVEN}
+fi
+
+#Jeżeli katalogi nie istnieją
+if [[ ! -d ${FIRST_DIRECTORY} && ! -d ${SECOND_DIRECTORY} ]]; then
+	exit ${DIRECTORIES_DO_NOT_EXIST}
+fi
+
+FILES2REMOVE=$(find ${DIRECTORY} -xtype l)
+${FILE} > ${FILES2REMOVE}
+
+find ${DIRECTORY} -xtype l -exec rm {} \;
+
+
+exit 0
+
+
